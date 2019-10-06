4da0ef18ff144e268497db54249fab3f16297659
Renne Nissinen <rennex@iki.fi>
2019-10-06 18:26:31 +0300

    Improve finding list's config path

--- mailtitmouse.rb
+++ mailtitmouse.rb
@@ -4,13 +4,19 @@ require "mail"
 
 class MailTitmouse
   attr_reader :mail
+  attr_accessor :config_dir
 
   def initialize(maildata)
     @mail = Mail.new(maildata)
+    @config_dir = "#{__dir__}/config"
   end
 
-  def find_config_path
-    "#{__dir__}/config/#{@mail.to.first}"
+  def find_config_path(recipient = Array(@mail.to).first)
+    # clean up slashes and ".." from the address
+    addr = recipient.gsub(/\//, "_")
+    raise "Invalid recipient address" unless addr =~ /\A[^@]+@[^@]+\.[^@]+\z/
+
+    File.join(@config_dir, "#{addr}.yml")
   end
 
   def run
--- test/mailtitmouse_test.rb
+++ test/mailtitmouse_test.rb
@@ -4,10 +4,11 @@ require_relative "helper.rb"
 require_relative "../mailtitmouse.rb"
 
 describe MailTitmouse do
-  MAILDATA = File.read("#{__dir__}/fixtures/examplemail.txt").freeze
+  INPUT_MAIL = File.read("#{__dir__}/fixtures/examplemail.txt").freeze
 
   before do
-    @mtm = MailTitmouse.new(MAILDATA)
+    @mtm = MailTitmouse.new(INPUT_MAIL)
+    @mtm.config_dir = "#{__dir__}/fixtures"
   end
 
   it "parses the input email" do
@@ -15,7 +16,25 @@ describe MailTitmouse do
   end
 
   it "finds the config file path" do
-    assert_match /\/titmouselist@example\.net$/, @mtm.find_config_path
+    assert_match /\/titmouselist@example\.net\.yml$/, @mtm.find_config_path
+  end
+
+  it "accepts well-formed email addresses" do
+    addr = "perfectly@formed.address"
+    assert_includes @mtm.find_config_path(addr), addr
+  end
+
+  it "refuses non-global email addresses" do
+    ["..", "../", "localuser", "almost@ok", "too@many@ats.foo"].each do |address|
+      assert_raises {
+        @mtm.find_config_path(address)
+      }
+    end
+  end
+
+  it "doesn't accept path traversal characters in addresses" do
+    addr = "foo/bar@what.ever"
+    refute_includes @mtm.find_config_path(addr), addr
   end
 
 end
